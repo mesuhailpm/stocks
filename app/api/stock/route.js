5afa589c3ad221fcc085d4f5ed75a8bef3086386
Mohammed Suhail PM <mohammedpm@meta.com>
2023-07-17 23:59:48 +0530

    add regex base search no tested

--- app/api/stock/route.js
+++ app/api/stock/route.js
@@ -5,14 +5,14 @@ export const GET = async (request) => {
         const query = request.nextUrl.searchParams.get('query')
         const db = await connectToDb()
         const stocks = db.collection('stocks')
-        const stocksData = await stocks.find({name:query}).toArray()
+        const stocksData = await stocks.find({name:{$regex: query}}).toArray()
 
         return new Response(JSON.stringify(stocksData), { status: 200 })
-        
-        
+
+
     } catch (error) {
         return new Response(error, { status:501})
-        
+
     }
 
 }
